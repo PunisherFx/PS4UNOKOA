0ad5df39c9c473634444b62013a3d60f6b948250
PunisherFx <kacedyouva19@gmail.com>
2025-03-11 01:47:43 +0100

    Maj

--- UnoPmetiers/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.unopmetiers {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.unopmetiers to javafx.fxml;
-    exports org.example.unopmetiers;
-}
